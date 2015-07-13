2f314c6f421f5a1b686d85311deb570b2a0ea1d6
Yogthos <dmitri.sotnikov@gmail.com>
2015-07-12 20:14:10 -0400

    renamed project

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject yogthos/iText "4.2.0"
+(defproject itext "4.2.0"
   :description "This is a build of the last LGPL/MPL version of iText."
   :url "https://github.com/yogthos/iText-4.2.0"
   :license {:name "GNU General Lesser Public License (LGPL) version 3.0"
-            :url "http://www.gnu.org/licenses/lgpl.html"
-            :comments "same as  iText and JFreeChart"}
+            :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[bouncycastle/bctsp-jdk14 "138"]
                  [org.jfree/jfreechart "1.0.15"]
                  [com.lowagie/itext "4.2.1"]
